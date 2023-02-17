dc025cf049da0a445523f6d0045ce43463756063
danhnh <danhnh@midota.com>
2023-02-17 16:12:37 +0700

    ttt1

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,12 @@ module.exports = defineConfig({
     themeColor: "#4DBA87",
     msTileColor: "#000000",
     appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "black"
+    appleMobileWebAppStatusBarStyle: "black",
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: './src/service-worker.js',
+      // ...other Workbox options...
+    }
   }
 })
