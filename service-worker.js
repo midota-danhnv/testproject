45c1f2e9f04b7c927c482cb53ac553263eb30bbd
danhnh <danhnh@midota.com>
2023-02-17 16:59:23 +0700

    21321321

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,7 @@ precacheAndRoute(self.__WB_MANIFEST);
 
 
 self.addEventListener('install', function(event) {
+  console.log(event)
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
