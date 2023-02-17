92ee994a76b579e231dfedca1f073f3847016534
danhnh <danhnh@midota.com>
2023-02-17 15:57:44 +0700

    tt

--- src/router.js
+++ src/router.js
@@ -1,15 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './components/Form/SimpleForm.vue'
-import PageLoad1 from "./components/Form/Page1.vue"
 const routes = [
     {
-        path: '/page1',
-        component: Home
-    },
-    {
         path: '/',
-        component: PageLoad1
+        component: Home
     },
 ]
 
