f5fea7d72c95bc133d4f0f9e05e164b7f13469ff
danhnh <danhnh@midota.com>
2023-02-17 16:44:51 +0700

    t34

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="container-fluid mt-3">
-    <SimpleForm />
+    <router-view/>
     </div>
 </template>
 
 <script>
-import SimpleForm from './components/Form/SimpleForm.vue'
 
 export default {
   name: 'App',
-  components: {
-    SimpleForm
-  }
 }
 </script>
